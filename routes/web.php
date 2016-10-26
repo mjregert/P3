ddf9e106c2f918258d874b01d2fd94f0987c5221
Michael Regert <mjr171@g.harvard.edu>
2016-10-25 20:00:07 -0500

    foundational controller for users

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Shortcut Notation - Intentionally commented out but shown for example */
+//Route::resource('users', 'UserController');
+
+/* Full Notatoin for all 7 routes */
+Route::get('/users', 'UserController@index')->name('users.index');
+Route::get('/users/create', 'UserController@create')->name('users.create');
+Route::post('/users', 'UserController@store')->name('users.store');
+Route::get('/users/{user}', 'UserController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UserController@edit')->name('users.edit');
+Route::put('/users/{user}', 'UserController@update')->name('users.update');
+Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
